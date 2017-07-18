a7e5bdd965db1a133a6c05a152e3d871c6690ef8
Pedro Nevado <pedro@didekin.es>
2017-07-18 12:44:29 +0200

    version didekindroid-a-0.4.12

--- terminal/buildtest.sh
+++ terminal/buildtest.sh
@@ -5,22 +5,16 @@
 [ $# -ne 3 ] && { echo "args count should be 3" 1>&2; exit 1;}
 
 # env_init initialized ENV, BRANCH, GITREMOTE.
-source ./terminal/env_init.sh  $1
+source ./terminal/env_init.sh  $1 $3
 SUITE="$2"
-VERSION="$GITREMOTE-$3"
 echo "Suite:" ${SUITE}
 
 ./gradlew clean
 
 if [ ${ENV} == "local" ] ; then
-    echo "Git: add"
+    echo "Git: add to localdev branch"
     git add .
     git commit -m "version $VERSION"
-fi
-
-if [ ${ENV} = "awspre" ] ; then
-    echo "Git: merge with local, push aws_pre ..."
-    git merge localdev  -m "version $VERSION"
 fi
 
 echo "Uninstalling com.didekindroid ..."
